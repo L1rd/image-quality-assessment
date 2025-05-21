94e83317372901db40ec1130e3bcedaa17daab2c
Artem Riabokon <artemriabokon17@gmail.com>
2025-05-21 14:31:54 +0300

    auth disabling

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { HashRouter , Routes, Route, Navigate } from 'react-router-dom'
 import { LoginPage, MainApp, SignupPage, WelcomePage } from './pages'
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const currentUser = sessionStorage.getItem('currentUser');
+  // const currentUser = sessionStorage.getItem('currentUser');
 
-  if (!currentUser) {
-    return <Navigate to="/login" replace />;
-  }
+  // if (!currentUser) {
+  //   return <Navigate to="/login" replace />;
+  // }
 
   return <>{children}</>;
 };
@@ -16,11 +16,11 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route index element={<WelcomePage />} />
+        {/* <Route index element={<WelcomePage />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route
-          path="/main-app"
+        index
           element={
             <ProtectedRoute>
               <MainApp />
